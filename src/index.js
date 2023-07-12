93aae5c76e1a91fd99fe95d8a845b78225394dc9
Dimekhin Maksym <dimehin-makc@i.ua>
2023-07-12 21:31:28 +0100

    1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import axios from 'axios';
+
+console.log(axios.isCancel('something'));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
